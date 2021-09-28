82e52b6d73dea8ef33a6bb2ff4002dc747ca6912
Dylan Barstow <dcbarstow7@gmail.com>
2021-09-28 14:08:49 -0500

    created manager position

--- Assets/js/manager.js
+++ Assets/js/manager.js
@@ -2,10 +2,18 @@ const inquirer = require('inquirer')
 const Employee = require('./employee.js')
 
 class Manager extends Employee {
-    constructor(name, id, email, officeNumber){
-        
-
+    constructor(name, id, email, managersOffice){
+        super(name, id, email)
+        this.managersOffice = managersOffice
     }
 
+    managersOfficeNumber(){
+        return this.managersOffice
+    }
 
+    getPosition() {
+        return "Manager"
+    }
 }
+
+module.exports = Manager
